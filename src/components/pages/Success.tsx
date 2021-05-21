725944d88e33ba2bcc17f56fea239697272f4b27
Chiro <sachinsinghroxx@gmail.com>
2021-05-21 23:00:39 +0530

    started routing movies

--- src/components/pages/Success.tsx
+++ src/components/pages/Success.tsx
@@ -1,11 +1,22 @@
-import {Link} from 'react-router-dom'
+import { Info } from "@material-ui/icons";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import { HashRouter, Switch, Route } from "react-router-dom";
+import NewMovies from "../UI//NewMovies";
+import MovieInfo from "../UI/MovieInfo";
+
+const client = new ApolloClient();
+
 const Success = () => {
   return (
-    <div>
-      Hello Sachin
-      <br />
-      <Link to="/">Navigate to Login</Link>
-    </div>
+    <HashRouter>
+      <ApolloProvider client={client}>
+        <Switch>
+          <Route exact path="/movies" component={NewMovies} />
+          <Route exact path="info/:id" component={MovieInfo} />
+        </Switch>
+      </ApolloProvider>
+    </HashRouter>
   );
 };
 
